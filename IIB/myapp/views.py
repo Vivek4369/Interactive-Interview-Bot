61244357af48a24269da0ec0cb46f0cf5258efdd
Vivek4369 <76645157+Vivek4369@users.noreply.github.com>
2022-12-27 20:35:40 +0530

    added user database

--- IIB/myapp/views.py
+++ IIB/myapp/views.py
@@ -4,6 +4,14 @@ from django.http import HttpResponse
 from myapp.functions.functions import handle_uploaded_file  
 from myapp.forms import StudentForm  
 
+import mysql.connector as sql
+fn=''
+ln=''
+mail=''
+pw=''
+cpw=''
+
+
 def demo(request):  
     if request.method == 'POST':  
         student = StudentForm(request.POST, request.FILES) 
@@ -26,27 +34,75 @@ def demo(request):
         return render(request,"demo.html",{'form':student})  
 
 def login(request):
+    global mail,pw
     if request.method == 'POST':
-        if request.POST['email']=='viv@gmail.com' and request.POST['password'] == 'viv123':
-            return render(request,"home.html")
-        else:
+        m = sql.connect(host='localhost', user='root', passwd='Viv@4369',database='iib')
+        cursor = m.cursor()
+        data = request.POST
+        for key,value in data.items():
+            if key=="email":
+                mail = value
+            if key =="password":
+                pw = value
+
+        cmd = "select * from users where email='{}' and password='{}'".format(mail,pw)
+        cursor.execute(cmd)
+        t = tuple(cursor.fetchall())
+        if t==():
             context = {
-                'msg': 'Wrong email or password'
+                'msg': 'wrong email or password'
             }
-            return render(request,"login.html",context)
+            return render(request,'login.html',context)
+        else:
+            return render(request,'home.html')
+
     else:
         return render(request,"login.html")
+
 def home(request):
     return render(request,"home.html")
+
 def features(request):
     return render(request,"features.html")
+
 def integration(request):
     return render(request,"integration.html")
+
 def resources(request):
     return render(request,"resources.html")
+
 def solutions(request):
     return render(request,"solutions.html")
+
 def registration(request):
-    return render(request,"registration.html")
+    global mail,pw 
+    global fn,ln,mail,pw,cpw 
+    if request.method == 'POST':
+        m = sql.connect(host='localhost', user='root', passwd='Viv@4369',database='iib')
+        cursor = m.cursor()
+        data = request.POST
+        for key,value in data.items():
+            if key=="firstname":
+                fn = value
+            if key =="lastname":
+                ln = value
+            if key=="email":
+                mail = value
+            if key =="password":
+                pw = value
+            if key =="cpassword":
+                cpw = value
+
+        cmd = "insert into users Values('{}','{}','{}','{}','{}')".format(fn,ln,mail,pw,cpw)
+        cursor.execute(cmd)
+        m.commit()
+        context = {
+            'msg': 'Sucessfully registered'
+        }
+        return render(request,'signup.html',context)
+
+    else:
+        return render(request,"signup.html")
+
 def result(request):
     return render(request,"result.html")
